0faa08f31b1f4aa228524f7015a6248dcb3569cd
Diogo M. Roque <roque.dmr@outlook.com>
2023-05-19 21:13:10 -0300

    Add files via upload

--- root.js
+++ root.js
@@ -12,58 +12,57 @@ navLinks.forEach((link) => {
   });
 });
 
-// Show or hide the scroll-to-top button based on scroll position
-window.addEventListener('scroll', function() {
-    const scrollTopButton = document.getElementById('scrollTopButton');
-    if (window.scrollY > 300) {
-      scrollTopButton.classList.add('visible');
-      scrollTopButton.classList.remove('hidden');
-    } else {
-      scrollTopButton.classList.add('hidden');
-      scrollTopButton.classList.remove('visible');
-    }
+// Check scroll position and show/hide the scroll-to-top button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var scrollTopButton = document.getElementById("scrollTopButton");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopButton.classList.remove("hidden");
+  } else {
+    scrollTopButton.classList.add("hidden");
+  }
+}
+
+// Smoothly scroll to the top of the page when the button is clicked
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
-  
-  // Scroll to the top of the page
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  }
-  
+}
 
+var text = "Full-Stack Developer"; // Text to be typed
+var speed = 100; // Typing speed (in milliseconds)
+var i = 0;
 
-  var text = "Full-Stack Developer"; // Texto que será digitado
-  var speed = 100; // Velocidade de digitação (em milissegundos)
-  var i = 0;
-  
-  function typeWriter() {
-    var element = document.getElementById("Fullstack");
-    if (i < text.length) {
-      element.innerHTML += text.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    } else {
-      setTimeout(eraseText, 2000);
-    }
+function typeWriter() {
+  var element = document.getElementById("Fullstack");
+  if (i < text.length) {
+    element.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(eraseText, 2000);
   }
-  
-  function eraseText() {
-    var element = document.getElementById("Fullstack");
-    if (i >= 0) {
-      var newText = element.innerHTML.slice(0, -1);
-      element.innerHTML = newText;
-      i--;
-      setTimeout(eraseText, speed);
-    } else {
-      setTimeout(typeWriter, 2000);
-    }
+}
+
+function eraseText() {
+  var element = document.getElementById("Fullstack");
+  if (i >= 0) {
+    var newText = element.innerHTML.slice(0, -1);
+    element.innerHTML = newText;
+    i--;
+    setTimeout(eraseText, speed);
+  } else {
+    setTimeout(typeWriter, 2000);
   }
-  
-  // Define uma altura fixa para o elemento
-  document.getElementById("Fullstack").style.height = "20px";
-  
-  // Chama a função pela primeira vez
-  typeWriter();
-  
+}
+
+// Set a fixed height for the element
+document.getElementById("Fullstack").style.height = "20px";
+
+// Call the function for the first time
+typeWriter();
